9ca6ec4754c0f7e3aa22217ae700b7ae98ee90dd
Ivan <st.ivan.k.k@gmail.com>
2025-04-03 10:52:05 +0300

    delete empty page attraction to compile build

--- app/attraction/[slug]/page.tsx
+++
@@ -1,5 +0,0 @@
-// function generateStaticParams() {}
- 
-export default function SingleAtractionPage() {
-  return <h1>Hello, Blog Post Page!</h1>
-}
---
+++ app/attraction/page.tsx
@@ -0,0 +1,5 @@
+// function generateStaticParams() {}
+ 
+export default function SingleAtractionPage() {
+  return <h1>Hello, Blog Post Page!</h1>
+}
