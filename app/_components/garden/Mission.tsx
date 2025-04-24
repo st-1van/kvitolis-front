7c2896d9d91224f97b99ff0c93a78c85bb8655f8
Ivan <st.ivan.k.k@gmail.com>
2025-04-24 16:16:41 +0300

    mission/accordeon

---
+++ app/_components/garden/Mission.tsx
@@ -0,0 +1,69 @@
+"use client";
+import { useState } from "react";
+
+type MissionProps = {
+  title: string;
+  data: DataProps[];
+};
+
+type DataProps = {
+  name: string;
+  desc: string;
+};
+
+export default function Mission({ title, data }: MissionProps) {
+  const [openIndex, setOpenIndex] = useState<number | 0>(0);
+
+  const toggleAccordion = (index: number) => {
+    setOpenIndex(openIndex === index ? 0 : index);
+  };
+
+  return (
+    <section className="mission">
+        <div className="mission__title">
+          <h2>{title}</h2>
+        </div>
+
+
+        <div className="accordion container">
+
+          <div className="accordion__base">
+            {data.map((item, index) => (
+              <div
+                key={index}
+                className={`accordion__item ${openIndex === index ? "open" : ""}`}
+              >
+                <button className="accordion__question" onClick={() => toggleAccordion(index)}>
+                  {item.name}
+                  <span className="accordion__icon">{openIndex === index ? "−" : "+"}</span>
+                </button>
+                <div
+                  key={index}
+                  style={{ maxHeight: openIndex === index ? "200px" : "0" }}
+                  className="accordion__answer mobile"
+                >
+                  <p>
+                    {item.desc}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+
+          <div className="accordion__answer desc">
+            {data.map((item, index)=>(
+              <div
+                key={index} 
+                className={`${openIndex === index ? "block" : "hidden"}`}
+              >
+                <p>
+                  {item.desc}
+                </p>
+              </div>
+            ))}
+          
+          </div>
+        </div>
+    </section>
+  );
+}
