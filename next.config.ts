c49cdf491e109f4ea3fe9d699f2d083f3f00984a
Ivan <st.ivan.k.k@gmail.com>
2025-06-09 22:27:44 +0300

    remote patterns for images

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  images: {
-      domains: ['127.0.0.1'],
+  // images: {
+  //     domains: ['127.0.0.1'],
+  // },
+   images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '1337',
+        pathname: '/uploads/**',
+      },
+    ],
   },
 };
 
