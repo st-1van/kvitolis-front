097838aa23c648808a8cc85d3130e31c8e3e5c5f
Ivan <st.ivan.k.k@gmail.com>
2025-08-06 15:26:51 +0300

    data update

--- app/_components/data/alleyData/actualData.tsx
+++ app/_components/data/alleyData/actualData.tsx
@@ -7,7 +7,8 @@ import treeData from './tree.json'
 if ((AlleyActualData.length !== peopleData.length)||(AlleyActualData.length !== treeData.length)) {
   console.warn('Кількість елементів у data.json і peopleData не збігається');
 }
-
+// мапляться актуальні дані статичі, але відповідно елементи мають бути в правильному порядку
+// додам адмінку з базою де будуть усі зв'язки
 const actualData = AlleyActualData.map((alley, index) => ({
   ...alley,
   famousPeople: peopleData[index] ?? [],
