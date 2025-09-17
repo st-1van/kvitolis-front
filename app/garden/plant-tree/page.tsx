2b7dfc32ef4d131778ae4199bee52aafd82cd018
Ivan <st.ivan.k.k@gmail.com>
2025-09-17 16:54:31 +0300

    callToDonate

--- app/garden/plant-tree/page.tsx
+++ app/garden/plant-tree/page.tsx
@@ -1,10 +1,17 @@
 import { Suspense } from 'react';
 import ChoseTreeWrapper from '../../_components/garden/choseForm/ChoseTreeWrapper';
+import CallToDonate from '@/app/_components/garden/CalltoDonate';
 
 export default function chooseTree() {
   return (
     <Suspense fallback={<div>Завантаження...</div>}>
       <ChoseTreeWrapper />
+      <CallToDonate
+        title="Або підтримайте наш проект"
+        btn="залишити донат"
+        slug='https://shorts.pb.ua/-/dd6a6af5'
+        desc="пам'ятайте, що кожен внесок важливий, незалежно від розміру"
+      />
     </Suspense>
   );
 }
