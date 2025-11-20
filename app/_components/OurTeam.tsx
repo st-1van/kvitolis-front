854353a4e80305813bb84207c08492d73d5e52a6
Ivan <st.ivan.k.k@gmail.com>
2025-11-20 16:28:00 +0200

    our team

--- app/_components/OurTeam.tsx
+++ app/_components/OurTeam.tsx
@@ -1,54 +1,110 @@
+import Image from "next/image";
 
 const teamMembers = [
     {
         name: "Олександр Подзізей",
+        role: ["Ідея", 'Стратегія'],
+        photo:'/assets/team/oleksandr_podzizey.jpg',
+    },
+    {
+        name: "Тетяна Пашковська", 
+        role: ["Проєктний менеджмент", 'Стратегія'],
+        photo:'/assets/team/tetyana_pashkovska.jpg',
+    },
+    {
+        name: "Любомир Сінчук", 
+        role: ["Ландшафтний Дизайн", "Візуалізація", 'Стратегія'],
+        photo:'/assets/team/lyubomyr_sinchuk.jpg',
+    },
+    {
+        name: "Світлана", 
+        role: ["Менеджмент", 'Стратегія', 'Контент'],
+        photo:'/assets/team/svitlana.jpg',
+    },
+    {
+        name: "кафедри фіолології, релігії, філософії, культурознавства, історії ВНУ імені Лесі Українки",
+        role: ["Наукові консультації"],
+        photo:'/assets/team/VNU_logo.jpg',
+    },
+    {
+        name: "Іван Стельмах",
+        role: ["Розробка сайту", 'Стратегія'],
+        photo:'/assets/team/ivan_stelmakh.jpg',
+    },
 
-        role: "Засновник та керівник проекту",
-        bio: "Олександр має понад 10 років досвіду у сфері ландшафтного дизайну та урбаністики. Він вірить у створення зелених просторів, які об'єднують людей.",
+    {
+        name: "Ганна Личак",
+        role: ["Брендинг і дизайн", 'Стратегія'],
+        photo:'/assets/team/Hanna_Lychak.jpg',
     },
     {
-        name: "Марія Петренко", 
-        role: "Менеджер проекту",
-        bio: "Марія спеціалізується на догляді за рослинами та має глибокі знання про місцеву флору. Вона відповідає за здоров'я та красу нашого саду.",
+        name: "Ірина Сінкевич",
+        role: ["Брендинг і дизайн", 'Контент'],
+        photo:'/assets/team/iryna_sinkevuch.jpg',
     },
-    {
-        name: "Любомир Ковальчук", 
-        role: "Ландшафтний Дизайн",
-        bio: "Любомир спеціалізується на догляді за рослинами та має глибокі знання про місцеву флору. Він відповідає за здоров'я та красу нашого саду.",
-    },
-    {
-        name: "Іван Іваненко",
-        role: "Розробка та підтримка",
-        bio: "Іван має понад 10 років досвіду у сфері ландшафтного дизайну та урбаністики. Він вірить у створення зелених просторів, які об'єднують людей.",
-    },
-    {
-        name: "Марія Фоменко", 
-        role: "Менеджер",
-        bio: "Марія спеціалізується на догляді за рослинами та має глибокі знання про місцеву флору. Вона відповідає за здоров'я та красу нашого саду.",
-    },
-    {
-        name: "Ганна Сидоренко",
-        role: "Брендинг і дизайн",
-        bio:'Ганна має понад 10 років досвіду у сфері ландшафтного дизайну та урбаністики. Вона вірить у створення зелених просторів',
-    }
+
 ]
+
+const rolesArray = [
+        'Ідея',
+        'Стратегія',
+        'Проєктний менеджмент',
+        'Брендинг і дизайн',
+        'Ландшафтний Дизайн',
+        'Візуалізація',
+        'Наукові консультації',
+        'Розробка сайту',
+        'Менеджмент',
+        'Контент'
+    ]
+
+const XY = [
+    {left: '300px', top: '-50px'},
+    {left: '85px', top: '65px'},
+    {left: '250px', top: '300px'},
+    {left: '450px', top: '390px'},
+    {left: '80vw', top: '0px'},
+    {left: '70vw', top: '360px'},
+    {left: '78vw', top: '245px'},
+    {left: '68vw', top: '200px'},
+]
+
+const membersArrayWithPositions = teamMembers.map((member, index) => ({
+    src: member.photo,
+    style: {
+        left: XY[index].left,
+        top: XY[index].top,
+    },
+    className: `animate up-and-down_${(index % 3) + 1}`,
+}));
 
 
 export function SimpleOurTeam(){
     return (
-        <section className="ourTeamSimple animate fade-in-up">
-            <div style={{maxWidth: '550px', margin: '0 auto', textAlign: 'left'}}>
+        <section className="ourTeam animate fade-in-up">
+            <div className="container">
+                <div className="ourTeam__content">
                 <h5>Команда проєкту:</h5>
-                <p>Ідея: {teamMembers[0].name}</p>
-                <p>Менеджер проєкту: {teamMembers[1].name}</p>
-                <p>Брендинг і дизайн: {teamMembers[5].name}</p>
-                <p>Стратегія: {`${teamMembers[0].name}, ${teamMembers[1].name}, ${teamMembers[5].name}`}</p>
-                <p>Ландшафтний дизайнер: {teamMembers[2].name}</p>
-                <p>Візуалізація: {teamMembers[2].name}</p>                
-                <p>Розробка та підтримка сайту: {teamMembers[3].name}</p>
-                <p>Менеджери: {teamMembers[4].name}</p>
-                <p>Наукові консультанти: {teamMembers[6]?.name ?? "Немає інформації"}</p>
-                <p>Наукова підтримка: кафедри фіолології, релігії, філософії, культурознавства, історії ВНУ імені Лесі Українки</p>
+                {rolesArray.map((role, index) => (
+                    <p key={index} className="ourTeam__roles">
+                        {role}: {teamMembers.filter(member => member.role.includes(role)).map(member => member.name).join(', ')}
+                    </p>
+                ))}
+                </div>
+            </div>
+            <div className="ourTeam__bg">
+                {membersArrayWithPositions.map((photo, index) => (
+                    <Image 
+                        src={photo.src} 
+                        key={index}
+                        alt={`Team member ${index + 1}`} 
+                        loading="lazy"
+                        className={photo.className}
+                        width={120} 
+                        height={120}    
+                        style={photo.style}
+                    />
+                ))}
             </div>
         </section>
     );
